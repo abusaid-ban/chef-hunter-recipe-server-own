952fd25eee71461826dc87ffe51dd8de39ba3bed
unknown <personal.abusaid@gmail.com>
2023-05-05 11:01:17 +0600

    create another json data

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const port = process.env.PORT || 5000
 
 const categories = require('./data/categories.json');
+const details = require('./data/details.json');
+
 app.use(cors())
 
 app.get('/', (req, res) => {
@@ -11,6 +13,15 @@ app.get('/', (req, res) => {
 })
 app.get('/categories', (req, res) => {
   res.send(categories)
+})
+app.get('/details', (req, res) => {
+  res.send(details)
+})
+app.get('/details/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedDetails = details.find(d=> d.id === id)
+  res.send(selectedDetails)
 })
 
 app.listen(port, () => {
